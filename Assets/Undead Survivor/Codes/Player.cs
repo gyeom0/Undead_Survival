fb37781b13a25d2e88091fd480b1c35a638e2a24
gyeom0 <121607278+gyeom0@users.noreply.github.com>
2025-05-17 22:20:15 +0900

    플레이어 이동

---
+++ Assets/Undead Survivor/Codes/Player.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 inputVec;
+    public float speed;
+
+    Rigidbody2D rigid;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        inputVec.x = Input.GetAxis("Horizontal");
+        inputVec.y = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+        // 3. 위치 이동
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+}

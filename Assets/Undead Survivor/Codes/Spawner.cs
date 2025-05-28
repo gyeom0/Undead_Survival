e70e8643cf52a676d8d46313452e1380fad1a658
gyeom0 <121607278+gyeom0@users.noreply.github.com>
2025-05-29 01:51:35 +0900

    소환 레벨 적용

--- Assets/Undead Survivor/Codes/Spawner.cs
+++ Assets/Undead Survivor/Codes/Spawner.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-
+  public SpawnData[] spawnData;
   public Transform[] spawnPoint;
+  int level;
   float timer;
   void Awake()
   {
@@ -14,7 +16,9 @@ public class Spawner : MonoBehaviour
   void Update()
   {
     timer += Time.deltaTime;
-    if (timer > 0.2f)
+    level = Mathf.Min(Mathf.FloorToInt(GameManager.instance.gameTime / 10f), spawnData.Length - 1);
+
+    if (timer > spawnData[level].spawnTime)
     {
       timer = 0f;
       Spawn();
@@ -22,7 +26,17 @@ public class Spawner : MonoBehaviour
   }
   void Spawn()
   {
-    GameObject enemy = GameManager.instance.pool.Get(Random.Range(0, 2)); //0에서 1(스켈레톤, 좀비)인덱스의 적을 무작위로 선택
+    GameObject enemy = GameManager.instance.pool.Get(0);
     enemy.transform.position = spawnPoint[Random.Range(1, spawnPoint.Length)].position; //1~n 중 랜덤 위치에 적 배치 //자기자신을 포함하기 때문에 0이 아니라 1부터
+    enemy.GetComponent<Enemy>().Init(spawnData[level]);
   }
+}
+[System.Serializable]
+public class SpawnData
+{
+  public float spawnTime;
+  public int spriteType;
+  public int health;
+  public float speed;
+
 }

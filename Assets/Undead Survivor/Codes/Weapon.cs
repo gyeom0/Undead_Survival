5bc6167deb9ccc86dc59c07e51c28a36152ef01b
gyeom0 <121607278+gyeom0@users.noreply.github.com>
2025-06-13 06:04:50 +0900

    능력 업그레이드 구현

--- Assets/Undead Survivor/Codes/Weapon.cs
+++ Assets/Undead Survivor/Codes/Weapon.cs
@@ -15,13 +15,9 @@ public class Weapon : MonoBehaviour
   Player player;
   void Awake()
   {
-    player = GetComponentInParent<Player>();
+    player = GameManager.instance.player;
   }
 
-  void Start()
-  {
-    Init();
-  }
   void Update()
   {
     switch (id)
@@ -56,20 +52,41 @@ public class Weapon : MonoBehaviour
     if (id == 0)
       Batch();
 
+    player.BroadcastMessage("ApplyGear", SendMessageOptions.DontRequireReceiver);
   }
 
-  public void Init()
+  public void Init(ItemData data)
   {
+    // Basic Set
+    name = "Weapon " + data.itemId;
+    transform.parent = player.transform;
+    transform.localPosition = Vector3.zero;
+
+    // Property Set
+    id = data.itemId;
+    damage = data.baseDamage;
+    count = data.baseCount;
+
+    for (int index = 0; index < GameManager.instance.pool.prefabs.Length; index++)
+    {
+      if (data.projectile == GameManager.instance.pool.prefabs[index])
+      {
+        prefabId = index;
+        break;
+      }
+    }
+
     switch (id)
     {
       case 0:
-        speed = -150;
+        speed = 150;
         Batch();
         break;
       default:
         speed = 0.3f;
         break;
     }
+    player.BroadcastMessage("ApplyGear", SendMessageOptions.DontRequireReceiver);
   }
 
   void Batch()
